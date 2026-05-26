89e65a03821ffe79aee25b340ca6bc6b6c4c1c9c
LucasFelixSilveira <lucasdwbfff@gmail.com>
2026-05-26 02:46:09 -0300

    chore(scripts): added android build script

--- scripts/aarch64-android-build.sh
+++ scripts/aarch64-android-build.sh
@@ -6,6 +6,6 @@ cd compiler
 clang++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions main.cpp \
     -o ../build/carla \
     -I. \
-    -L./libs/aarch64-linux \
+    -L./libs/aarch64-android \
     -leva
 cd ..
