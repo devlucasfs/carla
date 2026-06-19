74630dcae2b425912aef8da4e8608d819df9fdaf
Lucas Silveira <lucasfsil@proton.me>
2026-06-19 04:59:44 -0300

    chore(git): fixing git script to push

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,29 +1,45 @@
+#!/bin/bash
+
 PROJECT="Carla"
 
-echo git add .
+rm -rf uplogs
+
+echo "git add ."
 git add .
 
-echo git commit -m \"$1\"
+echo "git commit -m \"$1\""
 git commit -m "$1"
 
-echo git push origin main
+echo "git push origin main"
 git push origin main
 
 echo
 
-echo git clone git@github.com:devlucasfs/uplogs.git
+echo "git clone git@github.com:devlucasfs/uplogs.git"
 git clone git@github.com:devlucasfs/uplogs.git
 
-echo cd uplogs
-cd uplogs
+echo "cd uplogs"
+cd uplogs || exit
 
-(cat README.md | { cat; echo "\n# New commit in $PROJECT.\n\`\`\`\n$1\n\`\`\` "; } ) > README.md
+tmp=$(mktemp)
 
-echo git add .
+cat README.md > "$tmp"
+
+cat >> "$tmp" <<EOF
+# New commit in $PROJECT.
+- Date: $(date "+%Y-%m-%d %H:%M:%S")
+\`\`\`
+$1
+\`\`\`
+EOF
+
+mv "$tmp" README.md
+
+echo "git add ."
 git add .
 
-echo git commit -m \"update($PROJECT): $1\"
-git commit -m \"update($PROJECT): $1\"
+echo "git commit -m \"update($PROJECT): $1\""
+git commit -m "update($PROJECT): $1"
 
-echo git push origin main
+echo "git push origin main"
 git push origin main
